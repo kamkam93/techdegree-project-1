219a8fb9a27ace3ab9eaac6e25e971c433d3b84b
Brian Ball <brianbll9@gmail.com>
2018-08-08 07:51:18 -0400

    getRandomQuote

--- js/script.js
+++ js/script.js
@@ -7,12 +7,39 @@ var quotes = [
     quote: "Theatricality and deception, powerful agents to the uninitiated.",
     source: 'Bane',
     year: 2012
+  },
+  {
+    quote: "No, I am your father.",
+    source: 'Darth Vader',
+    year: 1980
+  },
+  {
+    quote: "Yes we can.",
+    source: 'Barack Obama',
+    year: 2008
+  },
+  {
+    quote: "Dude, what do you know about Romona Flowers?",
+    source: 'Scott Pilgrim',
+    year: 2010
+  },
+  {
+    quote: "Just keep swimming. Just keep swimming. Just keep swimming, swimming, swimming. What do we do? We swim, swim.",
+    source: 'Dory',
+    year: 2003
+  },
+  {
+    quote: "I'm going to make him an offer he can't refuse.",
+    source: 'The Godfather',
+    year: 1972
   }
-]
+];
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
+function getRandomQuote() {
 
+}
 
 // Create the printQuote funtion and name it printQuote
 

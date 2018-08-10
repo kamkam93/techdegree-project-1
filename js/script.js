2ceb83d55aa0bfaadc10f24993aaafd4f5778af4
Brian Ball <brianbll9@gmail.com>
2018-08-10 11:53:41 -0400

    notes

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ function getRandomQuote(array) {
 
 function printQuote() {
   var actualQuote = getRandomQuote(quotes); /* printQuote function calls getRandomQuote */
-  var stringOfQuoteProperties = "";
+  var stringOfQuoteProperties = ""; /* variable for html markup */
     stringOfQuoteProperties += "<p class="quote"> actualQuote.quote </p> + <p class="source"> actualQuote.source + actualQuote.year </p>" /* html markup */
   if (actualQuote.year.hasOwnProperty()) { /* checks Quotes for a quote with year property */
     stringOfQuoteProperties += "<span class="year"> actualQuote.year </span>";
